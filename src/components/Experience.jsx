0c6dc19c8dffe3c4fb5eff3dc59568f28a3253e7
bhav1kmaru <bhavik.m01@gmail.com>
2023-01-09 14:48:02 +0530

    done

---
+++ src/components/Experience.jsx
@@ -0,0 +1,17 @@
+import { Box, Heading,Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div>
+        <Heading>Experience</Heading>
+        <br></br>
+        <Box>
+            <Text as='b' fontSize='lg'>Front-End Intern at BluePen.co.in</Text>
+            <Text>May 2020-December 2020</Text>
+        </Box>
+    </div>
+  )
+}
+
+export default Experience
